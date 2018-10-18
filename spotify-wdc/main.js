c062e57e6aab91e14b097f97a38dcc3c1c428475
Skyler Johnson <skyler@Skylers-MacBook-Pro.local>
2018-10-18 15:09:59 -0400

    redirect update

--- spotify-wdc/main.js
+++ spotify-wdc/main.js
@@ -3,8 +3,8 @@
 var myConnector = tableau.makeConnector();
 
 var CLIENT_ID = "99772937ce6e47d08d9f1d26052404c3";
-// var REDIRECT_URI = "http://www.skybjohnson.com/spotify-wdc/";
-var REDIRECT_URI = "http://localhost:8000/";
+var REDIRECT_URI = "http://www.skybjohnson.com/spotify-wdc/";
+// var REDIRECT_URI = "http://localhost:8000/";
 var url = "https://accounts.spotify.com/authorize/?client_id=" + 
 CLIENT_ID + 
 "&response_type=token&redirect_uri=" + REDIRECT_URI + 
